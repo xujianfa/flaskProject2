b71554d2efb165d3d1973c84c2ecb1365d6dd2ea
xujianfa <421803497@qq.com>
2022-06-07 22:46:22 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'name': '123'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    print('wqeqw')
